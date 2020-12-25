d914dc4926fdb043e7dff01cfcfee5fdd565a90a
ucwong <ucwong@126.com>
2020-12-25 13:56:27 +0800

    get set added

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/", HelloHandler)
 
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/get", GetHandler)
+	http.HandleFunc("/set", SetHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Get invoke, %s!", r.URL.Path[1:])
+}
+
+func SetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Set invoke %s!", r.URL.Path[1:])
 }
