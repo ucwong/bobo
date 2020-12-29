41afef0f3a742a36bd7bd514c9b57665f28eebeb
ucwong <ucwong@126.com>
2020-12-29 16:18:14 +0800

    Ecdsa crypto added for sig

--- bench.sh
+++ bench.sh
@@ -1,7 +1,7 @@
-for i in {1..1000}
+for i in {1..1}
 do
-	curl -X POST -d "some data "$i http://localhost:8080/v2/example?k=1\&v=$i
+	curl -X POST -d "example info" http://localhost:8080/v2/example/0x2a2A0667f9cBF4055E48eaF0d5B40304B8822184?msg=aHellox\&sig=0xee78eaa27526b412d0e970b85f47c96aa0aa67ed1c06f577ffe712a91284659a0a38529194a53891c84919369e09bf7e08d1655544cb044671461e210ddad1eb00
 	echo ""
-	curl http://localhost:8080/v2/example?k=1
-	echo ""
+	#curl http://localhost:8080/v2/example/0xa19d069d48d2e9392ec2bb41ecab0a72119d633b
+	#echo ""
 done
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/CortexFoundation/CortexTheseus v1.10.13
 	github.com/dgraph-io/badger/v2 v2.0.1-rc1.0.20201223092806-3adc574c1490
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee
 )
